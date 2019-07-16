4f0027a5a02907f345b69978abb9bdb89ebb43b3
BodhaanshRavipati325 <45471123+BodhaanshRavipati325@users.noreply.github.com>
2019-07-15 22:38:34 -0500

    Added Hatch, Cargo, and Cargo Scoring Levels in constants (Set as 0). Created an empty command group for the Cargo Intake.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,4 +8,8 @@ public class Constants {
     public static final double armP = 0;
     public static final double armI = 0;
     public static final double armD = 0;
+
+    public static final double ballIntakeLevel = 0;
+    public static final double hatchIntakeLevel = 0;
+    public static final double cargoScoringLevel = 0;
 }
